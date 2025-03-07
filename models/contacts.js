aec8dbd05952ffd24db187fd8d330d5978beb863
Benedykt Powała <68753003+daszai@users.noreply.github.com>
2025-03-07 15:36:47 +0100

    1

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,94 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  return await fs
+    .readFile("contacts.json")
+    .then((data) => JSON.parse(data.toString()))
+    .catch((err) => console.log(err.message));
+};
 
-const getContactById = async (contactId) => {}
+const getById = async (contactId) => {
+  return await fs
+    .readFile("contacts.json")
+    .then((data) =>
+      JSON.parse(data.toString()).find((element) => {
+        return element.id === contactId;
+      })
+    )
+    .catch((err) => console.log(err.message));
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  let temp2 = false;
+  return await fs
+    .readFile("contacts.json")
+    .then((data) => {
+      const temp = JSON.parse(data.toString()).filter((element) => {
+        if (!(element.id === contactId)) {
+          return true;
+        } else {
+          temp2 = true;
+          return false;
+        }
+      });
+      fs.writeFile("contacts.json", JSON.stringify(temp));
+      if (temp2) {
+        return { status: 200, message: "contact deleted" };
+      }
+      return { status: 404, message: "Not found" };
+    })
+    .catch((err) => console.log(err.message));
+};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const temp = await fs.readFile("contacts.json");
+  const temp2 = JSON.parse(temp.toString());
+  temp2.push({
+    id: body.id,
+    name: body.name,
+    email: body.email,
+    phone: body.phone,
+  });
+  return await fs.writeFile("contacts.json", JSON.stringify(temp2));
+};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  let temp2 = false;
+  let obj;
+  return await fs
+    .readFile("contacts.json")
+    .then((data) => {
+      const temp = JSON.parse(data.toString()).map((element) => {
+        if (!(element.id === contactId)) {
+          return element;
+        } else {
+          temp2 = true;
+          if (body.name) {
+            element.name = body.name;
+          }
+          if (body.email) {
+            element.email = body.email;
+          }
+          if (body.phone) {
+            element.phone = body.phone;
+          }
+          obj = element;
+          return element;
+        }
+      });
+      fs.writeFile("contacts.json", JSON.stringify(temp));
+      if (temp2) {
+        return { status: 200, message: obj };
+      }
+      return { status: 404, message: "Not found" };
+    })
+    .catch((err) => console.log(err.message));
+};
 
 module.exports = {
   listContacts,
-  getContactById,
+  getById,
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,85 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
+const func = require("../../models/contacts");
+const id = require("uniqid");
+const Joi = require("@hapi/joi");
 
-const router = express.Router()
+const schema = Joi.object({
+  name: Joi.string().alphanum(),
+  email: Joi.string().email(),
+  phone: Joi.string(),
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const temp = await func.listContacts();
+  res.status(200).json(temp);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", async (req, res, next) => {
+  const temp = await func.getById(req.params.id);
+  if (temp) {
+    res.status(200).json(temp);
+  } else res.status(404).json({ message: "Not found" });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const { name, email, phone } = req.body;
+  const error3 = schema.validate({ email: email });
+  const error2 = schema.validate({ phone: phone });
+  const error1 = schema.validate({ name: name });
+  if (!error1.error && !error2.error && !error3.error) {
+    if (name && email && phone) {
+      req.body.id = id();
+      await func.addContact(req.body);
+      res.status(201).json({ id: req.body.id, name, email, phone });
+    } else {
+      res.status(400).json({ message: "missing required name - field" });
+    }
+  } else {
+    if (error1.error) res.status(400).json(error1.error);
+    if (error2.error) res.status(400).json(error2.error);
+    if (error3.error) res.status(400).json(error3.error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:id", async (req, res, next) => {
+  const temp = await func.removeContact(req.params.id);
+  res.status(temp.status).json({ message: temp.message });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:id", async (req, res, next) => {
+  const { name, email, phone } = req.body;
+  let error3 = { error: undefined };
+  let error2 = { error: undefined };
+  let error1 = { error: undefined };
+  console.log(phone);
+  if (email !== undefined) error3 = schema.validate({ email: email });
+  if (phone !== undefined) error2 = schema.validate({ phone: phone });
+  if (name !== undefined) error1 = schema.validate({ name: name });
 
-module.exports = router
+  if (!error1.error && !error2.error && !error3.error) {
+    if (name || email || phone) {
+      const { message, status } = await func.updateContact(
+        req.params.id,
+        req.body
+      );
+      if (message === "Not found")
+        res.status(status).json({ message: message });
+      else
+        res.status(status).json({
+          id: message.id,
+          name: message.name,
+          email: message.email,
+          phone: message.phone,
+        });
+    } else {
+      res.status(400).json({ message: "missing fields" });
+    }
+  } else {
+    if (error1.error) res.status(400).json(error1.error);
+    if (error2.error) res.status(400).json(error2.error);
+    if (error3.error) res.status(400).json(error3.error);
+  }
+});
+
+module.exports = router;
